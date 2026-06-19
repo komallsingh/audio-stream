9c4e20cfa054d697df42c8590a4a0ef517fe6d9f
komallsingh <singhkomal11711@gmail.com>
2026-06-19 22:45:12 +0530

    remember

--- app/src/main/java/com/komal/audioplay/AudioPlayerScreen.kt
+++ app/src/main/java/com/komal/audioplay/AudioPlayerScreen.kt
@@ -9,13 +9,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun AudioPlayerScreen(){
-    var isPlaying by mutableStateOf(false)
+    var isPlaying by remember{mutableStateOf(false)}
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
